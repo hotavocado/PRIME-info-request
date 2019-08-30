8aba5db875a3592c3a2b648f92efd18daf73b95f
Mike Xiao <mikemxxiao@gmail.com>
2019-08-30 10:45:31 -0400

    updated website with new survey data as of 8/30

--- Update_webpage.R
+++ Update_webpage.R
@@ -1,6 +1,6 @@
 require(rmarkdown)
 
-input_path <- "redcap_export/PremeetingRequestFor_DATA_2019-08-27_1756.csv"
+input_path <- "redcap_export/PremeetingRequestFor_DATA_2019-08-30_1417.csv"
 
 #Control script to run in order to update files for the website
 source('question_to_csv.R')
